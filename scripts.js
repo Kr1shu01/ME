7820481e6df95e18794159eacd1751ddd4c9c096
胡以明 <143874724+Kr1shu01@users.noreply.github.com>
2024-05-20 17:54:44 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,82 +1,120 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>領域展開！</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" href="images/logo.png" type="image/png"> 
-</head>
-<body>
-    <header>
-        <div class="nav-container">
-            <div class="button-container">
-                <div id="controls">
-                    <button class="play-pause-button">流音</button>
-                    <button class="next-button">迭曲</button>
-                </div>
-                <button class="ac-button" onclick="openAC()">风吟</button>
-                <button class="theme-button" onclick="toggleTheme()">易彩</button>
-                <button class="empty-button" onclick="">空按钮</button>
-                <button class="tv-button" onclick="openTV()">雅幕</button>
-                <button class="relax-button" onclick="startRelax()">寻秘</button>
-                <button class="dropdown-button" onclick="toggleDropdown()">鸣辞</button>
-                <div id="dropdown-menu" class="dropdown-menu">
-                    <a href="#" onclick="navigateToLink('https://www.bilibili.com/video/BV1xa411e7SC/?share_source=copy_web&vd_source=2fb33c4bf969e78e1d2f94a3f11bee75')">五月的花海</a>
-                    <a href="#" onclick="navigateToLink('https://mp.weixin.qq.com/s/VkcuAT4JkAW3a8F90HUiyQ')">启航未来</a>
-                    <a href="#" onclick="navigateToLink('https://v.qq.com/x/cover/mzc002002kqssyu/q4100vjztwh.html?j_vid=y41007hixcp&j_cut_vid=x4100fe2yzq&j_is_win_vid=1&j_start=0')">融媒体之歌</a>
-                </div>
-                <button class="console-button" onclick="openConsole()">颂境</button>
-            </div>
-            <div class="header-content">
-                <img src="images/Evelynn2.png" alt="头像">
-                <div>
-                    <h1>KRISHU</h1>
-                    <p>身不由己去打野</p>
-                </div>
-            </div>
-            <nav>
-                <a href="#about">个人简介</a>
-                <a href="#experience">工作经历</a>
-                <a href="#projects">项目展示</a>
-                <a href="#skills">专业技能</a>
-                <a href="#education">教育背景</a>
-                <a href="#hobbies">个人爱好</a>
-                <a href="#blog">博客文章</a>
-                <a href="#book-notes">读书摘录</a>
-                <a href="#music-festival">音乐盛典</a>
-                <a href="#civilization">晓以文明</a>
-            </nav>
-        </div>
-    </header>
-    <main>
-        <div class="content">
-            <div class="row">
-                <section id="about" class="about"><h2>个人简介</h2><p>在写作之路上踌躇前行，但是会一直热爱写作，哪怕山崩地裂，海枯石烂。</p></section>
-                <section id="experience" class="experience"><h2>工作经历</h2><p>研究生读得死去活来，但是乐在其中，只因深信学无止境。</p></section>
-            </div>
-            <div class="row">
-                <section id="projects" class="projects"><h2>项目展示</h2><p>上海市第一伊芙琳，第二莉莉娅，第三：想去哪儿就去哪儿。</p></section>
-                <section id="skills" class="skills"><h2>专业技能</h2><p>想学编曲，还想学滑雪，还有最关键的游泳。</p></section>
-            </div>
-            <div class="row">
-                <section id="education" class="education"><h2>教育背景</h2><p>再教育中，接受新知识的洗礼，争做新时代好青年。</p></section>
-                <section id="hobbies" class="hobbies"><h2>个人爱好</h2><p>如果李志不牛逼，那你猜背景音乐是谁喝醉唱的歌。</p></section>
-            </div>
-            <div class="row">
-                <section id="blog" class="blog"><h2>博客文章</h2><p>时间不留人，客官勿留步，游离在时间之门外，无法抬头，听从调遣。</p></section>
-                <section id="book-notes" class="book-notes"><h2>读书摘录</h2><p>余华别笑，你也有心事，总有一天你可以光明正大地说出来的。</p></section>
-            </div>
-            <div class="row">
-                <section id="music-festival" class="music-festival"><h2>音乐盛典</h2><p>啦啦啦哩啦，啦啦啦哩，啦啦啦哩啦啦。</p></section>
-                <section id="civilization" class="civilization"><h2>晓以文明</h2><p>科幻，晓以文明，晓之以文明，晓之以人类文明！</p></section>
-            </div>
-        </div>
-    </main>
-    <footer>
-        <p>联系方式：15979595635</p>
-    </footer>
+document.addEventListener('DOMContentLoaded', () => {
+    const playPauseButton = document.querySelector('.play-pause-button');
+    const nextButton = document.querySelector('.next-button');
+    const themeButton = document.querySelector('.theme-button');
+    const relaxButton = document.querySelector('.relax-button');
+    const acButton = document.querySelector('.ac-button');
+    const tvButton = document.querySelector('.tv-button');
+    const consoleButton = document.querySelector('.console-button');
+    const menuButton = document.querySelector('.menu-button');
+    const dropdownMenu = document.getElementById('dropdown-menu');
 
-    <script src="scripts.js"></script>
-</body>
-</html>
+    // 添加事件监听器
+    playPauseButton.addEventListener('click', togglePlayPause);
+    nextButton.addEventListener('click', playRandom);
+    themeButton.addEventListener('click', toggleTheme);
+    relaxButton.addEventListener('click', startRelax);
+    acButton.addEventListener('click', () => openLink('https://wxurl.cn/PME'));
+    tvButton.addEventListener('click', () => openLink('https://wxurl.cn/36C'));
+    consoleButton.addEventListener('click', () => openLink('http://yx.1dly.cn/'));
+    menuButton.addEventListener('click', toggleDropdown);
+
+    // 隐藏下拉菜单
+    document.addEventListener('click', function (event) {
+        if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.display = 'none';
+        }
+    });
+});
+
+const audio = new Audio();
+const mp3Files = [
+    'music/10_000_Hours.mp3', 'music/Blowin_In_the_Wind.mp3', 'music/Boyfriend.mp3', 'music/Cold_Water.mp3',
+    'music/Ghost.mp3', 'music/My_Back_Pages.mp3', 'music/No_Time_To_Die.mp3', 'music/Shake_It_Off.mp3',
+    'music/Six_Feet_Under.mp3', 'music/Style(Karaoke_Version).mp3', 'music/White_Christmas.mp3',
+    'music/Workingman\'s_Blues.mp3', 'music/aimei.mp3', 'music/aizaixiyuanqian.mp3', 'music/bad_guy.mp3',
+    'music/bandaotiehe.mp3', 'music/bugai.mp3', 'music/caihong.mp3', 'music/changhuan.mp3', 'music/chengaiying.mp3',
+    'music/congcongnanian.mp3', 'music/cuimian.mp3', 'music/dangni.mp3', 'music/daoxiang.mp3', 'music/erlingsan.mp3',
+    'music/fanfangxiangdezhong.mp3', 'music/feiyunzhixia.mp3', 'music/fushishanxia.mp3', 'music/ganbei.mp3',
+    'music/hongchenkezhan.mp3', 'music/huahai.mp3', 'music/huoyuanjia.mp3', 'music/idontwannabeyouanymore.mp3',
+    'music/jiandanai.mp3', 'music/jie.mp3', 'music/lantingxu.mp3', 'music/lizhichangzhou.mp3', 'music/longjuanfeng.mp3',
+    'music/mingmingjiu.mp3', 'music/ocean_eyes.mp3', 'music/peiniduguomanchangsuiyue.mp3', 'music/putaochengshushi.mp3',
+    'music/qilixiang.mp3', 'music/qingfengxulai.mp3', 'music/qingtian.mp3', 'music/renjian.mp3', 'music/shengxia.mp3',
+    'music/shuohaobuku.mp3', 'music/shuohaodexingfune.mp3', 'music/taotai.mp3', 'music/tingmamadehua.mp3',
+    'music/turanhaoxiangni.mp3', 'music/wenrou.mp3', 'music/wish_you_were_gay.mp3', 'music/woyouchulianle.mp3',
+    'music/xiangwozheyangderen.mp3', 'music/xiaochou.mp3', 'music/xiaojiuwo.mp3', 'music/xiaowangshu.mp3',
+    'music/xuanmu.mp3', 'music/yequ.mp3', 'music/yifuzhiming.mp3', 'music/yiluxiangbei.mp3', 'music/you_should_see_me_in_a_crown.mp3',
+    'music/youjianchuiyan.mp3'
+];
+let isPlaying = false;
+let isFirstPlay = true;
+
+audio.addEventListener('ended', playRandom);
+
+function togglePlayPause() {
+    if (isPlaying) {
+        audio.pause();
+        document.querySelector('.play-pause-button').textContent = '流音';
+    } else {
+        if (isFirstPlay) {
+            playRandom();
+            isFirstPlay = false;
+        } else {
+            audio.play();
+            document.querySelector('.play-pause-button').textContent = '驻乐';
+        }
+    }
+    isPlaying = !isPlaying;
+}
+
+function playRandom() {
+    const randomIndex = Math.floor(Math.random() * mp3Files.length);
+    audio.src = mp3Files[randomIndex];
+    audio.play();
+    document.querySelector('.play-pause-button').textContent = '驻乐';
+    isPlaying = true;
+}
+
+// 主题切换
+const themes = [
+    { bg: 'linear-gradient(90deg, #FF69B4, #FFB6C1)', secBg: 'linear-gradient(45deg, #FFCC99, #FFA07A)' },
+    { bg: 'linear-gradient(90deg, #00BFFF, #1E90FF)', secBg: 'linear-gradient(45deg, #ADD8E6, #87CEFA)' },
+    { bg: 'linear-gradient(90deg, #3CB371, #2E8B57)', secBg: 'linear-gradient(45deg, #98FB98, #00FA9A)' }
+];
+let currentTheme = 0;
+
+function toggleTheme() {
+    currentTheme = (currentTheme + 1) % themes.length;
+    const theme = themes[currentTheme];
+    document.body.style.background = theme.bg;
+    document.querySelector('header').style.background = theme.bg;
+    document.querySelector('main').style.background = theme.bg;
+    document.querySelector('.content').style.background = theme.bg;
+    document.querySelectorAll('.content section').forEach(section => section.style.background = theme.secBg);
+    document.querySelector('footer').style.background = theme.bg;
+}
+
+// 轻松一下按钮
+function startRelax() {
+    const games = [
+        'https://play2048.co/', 'https://playsnake.org/', 'https://hextris.io/', 'https://sudoku.com/',
+        'https://www.bubbleshooter.net/', 'https://liferestart.syaro.io/public/index.html', 'https://proxx.app/',
+        'https://pinball.flutter.dev/'
+    ];
+    window.open(games[Math.floor(Math.random() * games.length)], '_blank');
+}
+
+// 外部链接按钮
+function openLink(url) {
+    window.open(url, '_blank');
+}
+
+// 下拉菜单
+function toggleDropdown() {
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+function navigateToLink(videoLink) {
+    window.open(videoLink, '_blank');
+}
