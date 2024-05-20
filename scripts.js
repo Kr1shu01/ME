60a0b90827569b05f194fbae5cfd0f3ef68ec669
胡以明 <143874724+Kr1shu01@users.noreply.github.com>
2024-05-20 11:53:45 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('initial-play-button').addEventListener('click', initialPlay);
     document.querySelector('.play-pause-button').addEventListener('click', togglePlayPause);
-    document.querySelector('.next-button').addEventListener('click', playRandom);
+    document.querySelector('.next-button').addEventListener('click', playNext);
 
     document.querySelector('.theme-button').addEventListener('click', toggleTheme);
     document.querySelector('.relax-button').addEventListener('click', startRelax);
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.console-button').addEventListener('click', () => openLink('http://yx.1dly.cn/'));
 
     document.querySelector('.dropdown-button').addEventListener('click', toggleDropdown);
+    
     document.addEventListener('click', function(event) {
         const dropdownContainer = document.querySelector('.dropdown-container');
         const dropdownMenu = document.getElementById('dropdown-menu');
@@ -21,23 +22,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const audio = document.getElementById('background-audio');
 const mp3Files = [
-    'music/10_000_Hours.mp3','music/Blowin_In_the_Wind.mp3','music/Boyfriend.mp3','music/Cold_Water.mp3',
-    'music/Ghost.mp3','music/My_Back_Pages.mp3','music/No_Time_To_Die.mp3',
-    'music/Shake_It_Off.mp3','music/Six_Feet_Under.mp3','music/Style(Karaoke_Version).mp3','music/White_Christmas.mp3',
-    'music/Workingman\'s_Blues.mp3','music/aimei.mp3','music/aizaixiyuanqian.mp3',
-    'music/bad_guy.mp3','music/bandaotiehe.mp3','music/bugai.mp3','music/caihong.mp3',
-    'music/changhuan.mp3','music/chengaiying.mp3','music/congcongnanian.mp3','music/cuimian.mp3','music/dangni.mp3',
-    'music/daoxiang.mp3','music/erlingsan.mp3','music/fanfangxiangdezhong.mp3','music/feiyunzhixia.mp3',
-    'music/fushishanxia.mp3','music/ganbei.mp3','music/hongchenkezhan.mp3','music/huahai.mp3',
-    'music/huoyuanjia.mp3','music/idontwannabeyouanymore.mp3','music/jiandanai.mp3','music/jie.mp3',
-    'music/lantingxu.mp3','music/lizhichangzhou.mp3','music/longjuanfeng.mp3','music/mingmingjiu.mp3',
-    'music/ocean_eyes.mp3','music/peiniduguomanchangsuiyue.mp3','music/putaochengshushi.mp3',
-    'music/qilixiang.mp3','music/qingfengxulai.mp3','music/qingtian.mp3','music/renjian.mp3',
-    'music/shengxia.mp3','music/shuohaobuku.mp3','music/shuohaodexingfune.mp3','music/taotai.mp3',
-    'music/tingmamadehua.mp3','music/turanhaoxiangni.mp3','music/wenrou.mp3','music/wish_you_were_gay.mp3',
-    'music/woyouchulianle.mp3','music/xiangwozheyangderen.mp3','music/xiaochou.mp3','music/xiaojiuwo.mp3',
-    'music/xiaowangshu.mp3','music/xuanmu.mp3','music/yequ.mp3','music/yifuzhiming.mp3',
-    'music/yiluxiangbei.mp3','music/you_should_see_me_in_a_crown.mp3','music/youjianchuiyan.mp3'
+    'music/10_000_Hours.mp3', 'music/Blowin_In_the_Wind.mp3', 'music/Boyfriend.mp3', 'music/Cold_Water.mp3',
+    'music/Ghost.mp3', 'music/My_Back_Pages.mp3', 'music/No_Time_To_Die.mp3', 'music/Shake_It_Off.mp3',
+    'music/Six_Feet_Under.mp3', 'music/Style(Karaoke_Version).mp3', 'music/White_Christmas.mp3',
+    'music/Workingman\'s_Blues.mp3', 'music/aimei.mp3', 'music/aizaixiyuanqian.mp3', 'music/bad_guy.mp3',
+    'music/bandaotiehe.mp3', 'music/bugai.mp3', 'music/caihong.mp3', 'music/changhuan.mp3', 'music/chengaiying.mp3',
+    'music/congcongnanian.mp3', 'music/cuimian.mp3', 'music/dangni.mp3', 'music/daoxiang.mp3', 'music/erlingsan.mp3',
+    'music/fanfangxiangdezhong.mp3', 'music/feiyunzhixia.mp3', 'music/fushishanxia.mp3', 'music/ganbei.mp3',
+    'music/hongchenkezhan.mp3', 'music/huahai.mp3', 'music/huoyuanjia.mp3', 'music/idontwannabeyouanymore.mp3',
+    'music/jiandanai.mp3', 'music/jie.mp3', 'music/lantingxu.mp3', 'music/lizhichangzhou.mp3', 'music/longjuanfeng.mp3',
+    'music/mingmingjiu.mp3', 'music/ocean_eyes.mp3', 'music/peiniduguomanchangsuiyue.mp3', 'music/putaochengshushi.mp3',
+    'music/qilixiang.mp3', 'music/qingfengxulai.mp3', 'music/qingtian.mp3', 'music/renjian.mp3', 'music/shengxia.mp3',
+    'music/shuohaobuku.mp3', 'music/shuohaodexingfune.mp3', 'music/taotai.mp3', 'music/tingmamadehua.mp3',
+    'music/turanhaoxiangni.mp3', 'music/wenrou.mp3', 'music/wish_you_were_gay.mp3', 'music/woyouchulianle.mp3',
+    'music/xiangwozheyangderen.mp3', 'music/xiaochou.mp3', 'music/xiaojiuwo.mp3', 'music/xiaowangshu.mp3',
+    'music/xuanmu.mp3', 'music/yequ.mp3', 'music/yifuzhiming.mp3', 'music/yiluxiangbei.mp3',
+    'music/you_should_see_me_in_a_crown.mp3', 'music/youjianchuiyan.mp3'
 ];
 let isPlaying = false;
 
@@ -66,6 +66,10 @@ function playRandom() {
     document.querySelector('.play-pause-button').textContent = '暂停';
 }
 
+function playNext() {
+    playRandom();
+}
+
 // 主题切换
 const themes = [
     { bg: 'linear-gradient(90deg, #FF69B4, #FFB6C1)', secBg: 'linear-gradient(45deg, #FFCC99, #FFA07A)' },
@@ -73,7 +77,6 @@ const themes = [
     { bg: 'linear-gradient(90deg, #3CB371, #2E8B57)', secBg: 'linear-gradient(45deg, #98FB98, #00FA9A)' }
 ];
 let currentTheme = 0;
-document.querySelector('.theme-button').addEventListener('click', toggleTheme);
 
 function toggleTheme() {
     currentTheme = (currentTheme + 1) % themes.length;
@@ -87,7 +90,6 @@ function toggleTheme() {
 }
 
 // 轻松一下按钮
-document.querySelector('.relax-button').addEventListener('click', startRelax);
 function startRelax() {
     const games = [
         'https://play2048.co/',
@@ -103,26 +105,16 @@ function startRelax() {
 }
 
 // 外部链接按钮
-document.querySelector('.ac-button').addEventListener('click', () => openLink('https://wxurl.cn/PME'));
-document.querySelector('.tv-button').addEventListener('click', () => openLink('https://wxurl.cn/36C'));
-document.querySelector('.console-button').addEventListener('click', () => openLink('http://yx.1dly.cn/'));
 function openLink(url) {
     window.open(url, '_blank');
 }
 
 // 下拉菜单
-document.querySelector('.dropdown-button').addEventListener('click', toggleDropdown);
 function toggleDropdown() {
     const dropdownMenu = document.getElementById('dropdown-menu');
     dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 }
-document.addEventListener('click', function(event) {
-    const dropdownContainer = document.querySelector('.dropdown-container');
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    if (!dropdownContainer.contains(event.target)) {
-        dropdownMenu.style.display = 'none';
-    }
-});
+
 function navigateToLink(videoLink) {
     window.open(videoLink, '_blank');
 }
